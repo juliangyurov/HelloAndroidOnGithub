ccb576bc333c5490bebc6f38c0ca37a9adb158b6
julian.gyuroff@gmail.com <julian.gyuroff@gmail.com>
2022-10-19 21:31:25 +0300

    Second Commit:  Testing Log info message

--- app/src/main/java/com/julian/helloandroidongithub/MainActivity.java
+++ app/src/main/java/com/julian/helloandroidongithub/MainActivity.java
@@ -3,12 +3,15 @@ package com.julian.helloandroidongithub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    public final String TAG="MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG,"Testing new Log info message");
     }
 }
